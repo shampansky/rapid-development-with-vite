5301664fd3d9f02e67214657a0ae2ad4ebd388ef
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-11-17 20:20:39 +0300

    glob function

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// const autoImportedModules = import.meta.globEager('./autoImports/*.js')
+const autoImportedModules = import.meta.glob('./autoImports/*.js')
+
+console.log(autoImportedModules)
+
+for (const path in autoImportedModules) {
+  autoImportedModules[path]()
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
